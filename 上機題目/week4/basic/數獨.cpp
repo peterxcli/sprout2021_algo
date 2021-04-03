0a529d6734f95ed240d46eb855fac40065d06c4f
Peter Li <peterxcli@gmail.com>
2021-04-03 13:12:49 +0800

    我弱

--- 上機題目/week4/basic/數獨.cpp
+++ 上機題目/week4/basic/數獨.cpp
@@ -5,67 +5,70 @@ using namespace std;
 string input;
 char grid[9][9];
 
-bool ck(){
+bool check_all() {
 	int p[9];
 	fill(p,p+9,9);
-	for(int i=0;i<9;i++){
-		for(int j=0;j<9;j++){
-			p[grid[i][j]-49]-=1;
+	for(int i = 0; i < 9; i++){
+		for(int j = 0; j < 9; j++){
+			p[grid[i][j]-49] -= 1;
 		}
 	}
-	for(int j=0;j<9;j++){
-		if(p[j]>0)return false;
+	for(int j = 0;j < 9; j++){
+		if(p[j] > 0)return false;
 	}
 	return true;
 }
+
 bool check(int x,int y,char num){
-	for(int i=0;i<9;i++){
-  		if(grid[x][i]==num)return false;
+	for(int i = 0; i < 9; i++){
+  		if(grid[x][i] == num) return false;
   	}  
   	for(int i=0;i<9;i++){
-  		if(grid[i][y]==num)return false;
+  		if(grid[i][y] == num) return false;
 	}
-  	int blockx=x/3*3,blocky=y/3*3;
-  	for(int i=0;i<3;i++){
-  		for(int j=0;j<3;j++){
-  			if(grid[blockx+i][blocky+j]==num)return false;
+  	int blockx = (x / 3) * 3, blocky = (y / 3) * 3;
+  	for(int i = 0; i < 3; i++) {
+  		for(int j = 0; j < 3; j++) {
+  			if(grid[blockx + i][blocky + j] == num) return false;
 		}
 	}  	  
   	return true;
 }
-bool search(int wh){
-	int x=wh/9,y=wh%9;		 
-	if(wh==81)return true;
-	if(grid[x][y]!='.')return search(wh+1);
-	for(int i=0;i<9;i++){
-        char num=49+i;        
-        if(check(x,y,num)){        	
-		    grid[x][y]=num;
-            if(search(wh+1))return true;            
-			grid[x][y]='.';
+
+bool search(int wh) {
+	int x = wh / 9, y = wh % 9;		 
+	if(wh == 81) return true;
+	if(grid[x][y] != '.') return search(wh+1);
+	for(int i = 0; i < 9; i++) {
+        char num = 49+i; 
+        if(check(x, y, num)) {        	
+		    grid[x][y] = num;
+            if(search(wh+1)) return true;            
+			grid[x][y] = '.';
         }
     }
     return false;
 }
+
 int main(){
-	cin.tie(NULL); 
-	while(cin>>input&&input!="end"){
-		int k=0;
-		for(int i=0;i<9;i++){
-			for(int j=0;j<9;j++){
-				grid[i][j]=input[k];
-				k++;			
-			}				
+	cin.tie(NULL), ios_base::sync_with_stdio(0);
+	while(cin >> input && input != "end") {
+		int k = 0;
+		for(int i = 0; i < 9; i++) {
+			for(int j = 0; j < 9; j++){
+				grid[i][j] = input[k];
+				k++;
+			}
 		}
-		if(search(0) && ck()){
-			for(int i=0;i<9;i++){
-				for(int j=0;j<9;j++){
-					cout<<grid[i][j];
+		if(search(0) && check_all()) {
+			for(int i = 0;i < 9; i++){
+				for(int j = 0; j < 9; j++) {
+					cout << grid[i][j];
 				}
 			}
-			cout<<"\n";
+			cout << "\n";
 		}
-		else cout<<"No solution.\n";
+		else cout << "No solution.\n";
 	}
 	return 0;
 }
