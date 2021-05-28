19ea97c63b33c949ab33e50045983a34bfbfc00b
Peter Li <peterxcli@gmail.com>
2021-05-29 01:27:59 +0800

    HW 11 finish

--- 上機題目/week11/basic/陣線推進.cpp
+++ 上機題目/week11/basic/陣線推進.cpp
@@ -1,21 +1,55 @@
-#define MANY_SUBTASK
-#pragma GCC optimize(3, "Ofast", "inline")
 #include <bits/stdc++.h>
-#define endl '\n'
-#define int long long
+#define N 100005
 using namespace std;
 int n, m;
-void solve_subtask() {
+int in[N];
+int path[N];
+int sum = 0;
+vector<int> G[N];
+void Topsort() {
+    priority_queue<int, vector<int>, greater<int>> Q;
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+        if (in[i] == 0)
+            Q.push(i);
+
+    while (!Q.empty()) {
+        int x = Q.top();
+        Q.pop();
+        sum++;
+        path[++cnt] = x;
+        for (int i = 0; i < G[x].size(); i++) {
+            int y = G[x][i];
+            in[y]--;
+
+            if (in[y] == 0)
+                Q.push(y);
+        }
+    }
 }
-int32_t main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int cnt = 1;
-#ifdef MANY_SUBTASK
-    cin >> cnt;
-#endif
-    while (cnt--) {
-        solve_subtask();
+int main() {
+    cin.tie(0), ios_base::sync_with_stdio(0);
+    int T;
+    cin >> T;
+    while (T--) {
+        cin >> n >> m;
+        memset(in, 0, sizeof(in));
+        sum = 0;
+        for (int i = 0; i < n; i++)
+            G[i].clear();
+        for (int i = 1; i <= m; i++) {
+            int x, y;
+            cin >> x >> y;
+            G[x].push_back(y);
+            in[y]++;
+        }
+        Topsort();
+        if (sum != n) {
+            cout << "QAQ\n";
+            continue;
+        }
+        for (int i = 1; i <= n; i++)
+            cout << path[i] << (i!=n?" ":"\n");
     }
     return 0;
 }
--- 上機題目/week11/basic/高棕櫚的意外收穫.cpp
+++ 上機題目/week11/basic/高棕櫚的意外收穫.cpp
@@ -1,10 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-//#define endl '\n'
-#define int long long
+ 
+int m, a, b;
+multiset <int> st[505];
+vector <int> ans;
+ 
+void dfs(int x) {
+    while (st[x].size()){
+        int nxt = *st[x].begin();
+        st[x].erase(st[x].find(nxt));
+        st[nxt].erase(st[nxt].find(x));
+        dfs(nxt);
+    }
+    ans.push_back(x);
+}
+ 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    return 0;
+    while (cin >> m) {
+        if (m == 0) break;
+        ans.clear();
+        for (int i = 0; i < m; i++){
+            cin >> a >> b;
+            st[a].insert(b);
+            st[b].insert(a);
+        }
+        int start = 1;
+        for (int i = 1; i <= 500; i++){
+            if (st[i].size() & 1){
+                start = i;
+                break;
+            }
+        }
+        dfs(start);
+        reverse(ans.begin(), ans.end());
+        for (auto i:ans){
+            cout << i << "\n";
+        }
+        cout << "\n";
+    }
 }
